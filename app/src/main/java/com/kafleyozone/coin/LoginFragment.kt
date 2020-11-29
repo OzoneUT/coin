e02415b209e8c4b2b2cf2e8cfb54319cebe2c96c
Ozone Kafley <kafleyozone@gmail.com>
2020-11-29 02:55:00 -0600

    Built Login UI and updated the app icon

--- app/src/main/java/com/kafleyozone/coin/LoginFragment.kt
+++ app/src/main/java/com/kafleyozone/coin/LoginFragment.kt
@@ -3,4 +3,7 @@ package com.kafleyozone.coin
 import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    companion object {
+        const val TAG = "LoginFragment"
+    }
 }
--- app/src/main/java/com/kafleyozone/coin/RegistrationFragment.kt
+++ app/src/main/java/com/kafleyozone/coin/RegistrationFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.google.android.material.textfield.TextInputEditText
 import com.kafleyozone.coin.databinding.FragmentRegistrationBinding
@@ -28,6 +29,12 @@ class RegistrationFragment(private val pagerListener: OnboardingFlowFragment.Pag
         binding.registerButton.setOnClickListener {
             binding.registrationScrollview.smoothScrollTo(0, 0)
             onRegisterTapHandler(view)
+        }
+
+        binding.loginOnRegisterButton.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.fragment_container_view, LoginFragment(), LoginFragment.TAG)
+            }
         }
 
         viewModel.registrationSuccessState.observe(viewLifecycleOwner) {
